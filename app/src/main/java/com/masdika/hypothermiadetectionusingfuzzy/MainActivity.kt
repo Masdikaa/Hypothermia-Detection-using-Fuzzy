2cae54869a276d66fc651f921e875922d5b3bdd4
Masdikaa <masdikailhanmansiz@gmail.com>
2024-05-25 02:18:23 +0700

    Material3 Design

--- app/src/main/java/com/masdika/hypothermiadetectionusingfuzzy/MainActivity.kt
+++ app/src/main/java/com/masdika/hypothermiadetectionusingfuzzy/MainActivity.kt
@@ -3,16 +3,18 @@ package com.masdika.hypothermiadetectionusingfuzzy
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.masdika.hypothermiadetectionusingfuzzy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
